5fa0d7f9de7eeaa5138682839283a0db8799a549
sairam0125 <sairambola@gmail.com>
2025-01-24 10:30:20 +0530

    day 11

---
+++ day11/day11.js
@@ -0,0 +1,29 @@
+//!
+//  let ele= document.getElementsByClassName("test");
+//console.log(ele);
+//console.log(Array.isArray(ele));
+//  ele[0].style.backgroundColor="yellow"
+//! !spread operator:- it  is used to takeout each and every value from the original and
+//! syntax:- [...variable]
+
+//  let x=[...ele]
+//  console.log(x,Array.isArray(x));
+//  x.map((element)=>{
+//      element.style.backgroundColor="teal";
+//      })
+
+//! document.getElementByTagName();
+// let ele=document.getElementsByTagName("div");
+// console.log(ele);
+
+//! document.querySelector();
+// let ele=document.querySelector(".test");
+// console.log(ele);
+
+// let ele=document.querySelectorAll(".test");
+// [...ele].map(element=>{
+//     console.log(element.innerText);
+// })
+
+const btn=document.getElementById("btn");
+// btn.addEventListener("event","callbackfn")
